a8b8ef7a4acfed75e1b354407b73929ed81b9e7f
Alexei <kLaSs1k1806@gmail.com>
2022-07-17 00:11:18 +0300

    Exp003

---
+++ Exp003/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Random random = new Random();
+    int finishnumber = random.Next(1, 11);
+    int startnumber = 1;
+    Console.WriteLine(finishnumber);
+    while (startnumber <= finishnumber)
+    {
+        Console.WriteLine((Math.Pow(startnumber, 3)));
+        startnumber++;
+    }
